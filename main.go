cf9b4904e9b7d77a4561ed285b74d0b46a24adf0
Vitor Kuss <52881816+vkuss@users.noreply.github.com>
2023-04-10 15:52:25 -0300

    Update main.go

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		nodeName := os.Getenv("MY_NODE_NAME")
+		nodeName := os.Getenv("NODE_NAME")
 		fmt.Fprintf(w, "Hello, World! This is running on node %s\n", nodeName)
 	})
 	http.ListenAndServe(":8080", nil)
